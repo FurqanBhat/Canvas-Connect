c585d5a16f1dafdfcc4d4315d4480a2e5e8f2f83
Yassine Oudjana <y.oudjana@protonmail.com>
2024-04-03 10:57:22 +0300

    courses_model: Create course class
    Create a course class and populate it with data retrieved from the API. This
    provides isolation from the API and will allow for inserting custom processing
    in the future as needed.

--- lib/models/CoursesModel.dart
+++ lib/models/CoursesModel.dart
@@ -1,22 +1,39 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:provider/provider.dart';
 
 import 'LoginModel.dart';
+
+class Course {
+  String uuid;
+  String code;
+  String name;
+  Color color;
+
+  Course({required this.uuid, required this.code, required this.name, required this.color});
+}
+
 class CoursesModel{
   static bool requestSuccess=false;
   static Map<String, String> coursesName={};
-  static List<dynamic> courses=[];
-  static List<dynamic> allCourses=[];
+  static List<dynamic> courseData=[];
+  static List<dynamic> allCourseData=[];
+
+  static List<Course> courses = [];
+
   static Future<void> fetchCourses() async{
     final response= await get(Uri.parse("https://${LoginModel.domain}/api/v1/courses?per_page=50&access_token=${LoginModel.token}"));
     if(response.statusCode==200){
       requestSuccess=true;
       try{
-        courses=jsonDecode(response.body);
+        courseData=jsonDecode(response.body);
         setCourses();
         setCoursesName();
+
+        print(courseData);
+        print(coursesName);
       }
       catch(e){
         print(e.toString() +"my error");
@@ -34,10 +51,14 @@ class CoursesModel{
     print(now);
     int currMonth=now.month;
     int currYear=now.year;
+
+    /* Remove any courses left from previous logins */
+    courses = [];
+
     List temp=[];
     temp.addAll(courses);
-    for(final course in courses){
-      allCourses.add(course);
+    for(final course in courseData){
+      allCourseData.add(course);
       String date_created=course["start_at"];
       int yearCreated=int.parse(date_created.substring(0,4));
       int monthCreated=int.parse(date_created.substring(5,7));
@@ -47,12 +68,22 @@ class CoursesModel{
         temp.remove(course);
       }else if((currYear-yearCreated)>1){
         temp.remove(course);
+      } else {
+        courses.add(
+          Course (
+            uuid: course["uuid"],
+            code: course["course_code"],
+            name: course["name"],
+            /* TODO: Get user-defined color from Canvas when available */
+            color: Colors.primaries[Random().nextInt(Colors.primaries.length)]
+          )
+        );
       }
     }
-    courses=temp;
+    courseData=temp;
   }
   static void setCoursesName(){
-    for(final course in courses){
+    for(final course in courseData){
       coursesName["course_${course["id"]}"]=course["name"];
     }
   }
